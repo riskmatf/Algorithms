1bfe1f6d26cd280f5780176ceb9f64c1cb6fe5fa
Vuk Novakovic <vnovakovic96@gmail.com>
2017-09-21 03:19:37 +0200

    inital commit

---
+++ Theory/01-05-B-Tree/btree.h
@@ -0,0 +1,6 @@
+typedef struct node{
+	int num_of_keys;
+	int *values;
+	struct node**p; // mislim da ce ovo biti malo drugacije:W
+
+}Node;
---
+++ Theory/01-05-B-Tree/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "btree.h"
+
+
+int main(void){
+	printf("Jos uvek nista :(((");
+	
+	return 0;
+}
+
